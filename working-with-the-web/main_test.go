da4500fba059732d94866f8fd4f66c3c8c003772
Gill Erick <erick@sendyit.com>
2022-07-31 22:37:01 +0300

    Fix failing test

--- working-with-the-web/main_test.go
+++ working-with-the-web/main_test.go
@@ -27,28 +27,35 @@ func Test_checkError(t *testing.T) {
 }
 
 func Test_toursFromJson(t *testing.T) {
-	type args struct {
-		content string
+	content := `[
+	{
+		"tourId": "14",
+		"packageId": "5",
+		"packageTitle": "From Desert to Sea",
+		"name": "2 Days Adrift the Salton Sea",
+		"blurb": "The Salton Sea, 25% saltier than the Pacific, has been a tourist destination since the 1920s. See what attracts people to this desert oasis.",
+		"description": "The Salton Sea is saltier than the Pacific, an unusual feat for inland body of water. And even though its salinity has risen over the years, due in part to lack of outflows and pollution from agricultural runoff, it has attracted a small, but dedicated population. The sea itself offers recreational opportunities including boating, camping, off-roading, hiking, use of personal watercraft, photography and bird watching. The sea has been termed a \"crown jewel of avian biodiversity,\" being a major resting stop on the Pacific Flyway, a migratory path for birds. 2 Days Adrift the Salton Sea includes two nights accommodations at the Bombay Beach Inn, boat rental at the Salton City Harbor, and a guided fishing tour.",
+		"price": "350",
+		"difficulty": "2",
+		"length": "2",
+		"graphic": "map_saltonsea.gif",
+		"region": "Southern California"
 	}
-
-	content, err := ioutil.ReadFile("./working-with-the-web/sampleJsonResponse.json")
-	if err != nil {
-		panic(err)
-	}
+]`
 
 	tests := []struct {
-		name string
-		args args
-		want []Tour
+		name    string
+		content string
+		want    []Tour
 	}{
-		{"correctly parse JSON to Tour struct", args{string(content)}, []Tour{{
+		{"correctly parse JSON to Tour struct", content, []Tour{{
 			Name:  "2 Days Adrift the Salton Sea",
 			Price: "350",
 		}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := toursFromJson(tt.args.content); !reflect.DeepEqual(got, tt.want) {
+			if got := toursFromJson(content); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("toursFromJson() = %v, want %v", got, tt.want)
 			}
 		})
