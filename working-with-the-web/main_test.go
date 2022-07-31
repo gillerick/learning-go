951220fb04bf825a888274aa6e6813fc3b69124e
Gill Erick <erick@sendyit.com>
2022-07-31 14:03:00 +0300

    Add tests for working with web

--- working-with-the-web/main_test.go
+++ working-with-the-web/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io/ioutil"
 	"reflect"
 	"testing"
 )
@@ -9,11 +10,15 @@ func Test_checkError(t *testing.T) {
 	type args struct {
 		err error
 	}
+
+	_, err := ioutil.ReadFile("non-existing-file.txt")
+	_, errr := ioutil.ReadFile("./fromString.txt")
 	tests := []struct {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+		{"return error for non existing file", args{err}},
+		{"return error for non existing file", args{errr}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -25,12 +30,21 @@ func Test_toursFromJson(t *testing.T) {
 	type args struct {
 		content string
 	}
+
+	content, err := ioutil.ReadFile("./working-with-the-web/sampleJsonResponse.json")
+	if err != nil {
+		panic(err)
+	}
+
 	tests := []struct {
 		name string
 		args args
 		want []Tour
 	}{
-		// TODO: Add test cases.
+		{"correctly parse JSON to Tour struct", args{string(content)}, []Tour{{
+			Name:  "2 Days Adrift the Salton Sea",
+			Price: "350",
+		}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
